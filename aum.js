ef93e4e16922ea9be6d3747bc95787c4dc0761da
Bruno <brunetton@gmail.com>
2010-01-25 20:55:38 +0100

    Initial version
    Only center images are treated, not bottom ones on the caroussel

---
+++ aum.js
@@ -0,0 +1,6 @@
+$("table.a").each(function () {
+  nodeContent = jQuery($(this))[0].outerHTML
+  url = (/onclick="window.location='(\S+)'/).exec(nodeContent)[1]
+  $(this).removeAttr('onclick')
+  $(this).wrap('<a href="' + url + '"/>')
+});
