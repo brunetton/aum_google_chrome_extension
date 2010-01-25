564e5506962201178e4e89c83dafa9531ed2cb98
Bruno <brunetton@gmail.com>
2010-01-25 21:32:36 +0100

    directory structure update

--- aum_extension/aum.js
+++ aum_extension/aum.js
@@ -1,6 +1,8 @@
+// Center photos
 $("table.a").each(function () {
   nodeContent = jQuery($(this))[0].outerHTML
   url = (/onclick="window.location='(\S+)'/).exec(nodeContent)[1]
   $(this).removeAttr('onclick')
   $(this).wrap('<a href="' + url + '"/>')
 });
+
