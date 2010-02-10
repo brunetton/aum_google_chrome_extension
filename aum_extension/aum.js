3844928a9b3b97f2aa5e1cb87a64384965c16ef4
Bruno <brunetton@gmail.com>
2010-02-10 23:41:49 +0100

    - new center table naming - adding top logo, chat, top photos,

--- aum_extension/aum.js
+++ aum_extension/aum.js
@@ -1,8 +1,21 @@
 // Center photos
-$("table.a").each(function () {
-  nodeContent = jQuery($(this))[0].outerHTML
-  url = (/onclick="window.location='(\S+)'/).exec(nodeContent)[1]
-  $(this).removeAttr('onclick')
-  $(this).wrap('<a href="' + url + '"/>')
+$(".mini").each(function () {
+  make_clickable_link($(this));
 });
 
+// top logo, chat, top photos,
+$(".a[onclick]").each(function () {
+  make_clickable_link($(this));
+});
+
+function make_clickable_link(e) {
+  nodeContent = jQuery(e)[0].outerHTML
+   url = (/onclick="window.location='(\S+)'/).exec(nodeContent)[1]
+  e.removeAttr('onclick')
+  e.wrap('<a href="' + url + '"/>')
+}
+
+// debug
+function red(e) {
+  e.css("border", "solid 2px red");
+}
